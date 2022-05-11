97a01666514aba0e5125dd976a449918177da389
Mich Labs <88068139+MichaelGoonzalez@users.noreply.github.com>
2022-05-11 08:58:30 -0500

    Create Camera

--- code/level.py
+++ code/level.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 import pygame
 from settings import *
 from tile import Tile
@@ -9,7 +10,7 @@ class Level:
         #get display surfaces
         self.display_surface = pygame.display.get_surface()
         #Sprite Group Setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
         #sprite setup
         self.create_map()
@@ -26,6 +27,24 @@ class Level:
     
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.direction)
+        
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+    def custom_draw(self,player):
+        
+        #getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
